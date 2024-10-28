50cabddd9c45901b86801ace79d6a9cfedafa068
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-10-28 13:55:36 -0400

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
