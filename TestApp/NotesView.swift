8da0346ecea59f6998d1dfeb7353993052c42e08
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-10-29 16:59:07 -0400

    Focus App - ver - 1

---
+++ TestApp/NotesView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesView.swift
+//  TestApp
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 10/28/24.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesView()
+}
